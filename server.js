3f0952ea4a9a74b0d08f1da1e2f57c48e136a8ab
Dean Marques <deanmarquesjr@gmail.com>
2023-04-16 10:36:26 -0700

    update server.js for render

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
+
+
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
